dd700e35e03f66803f2c75516fad4854ba780b4c
M. Haris <muhammadharisaslam1@gmail.com>
2023-12-18 18:11:27 +0500

    Footer done

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('.youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+});
+
+
+
+
+
+
+
+
